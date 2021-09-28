df5ea07def37616088b56a2c7e5162c91f9c6f8c
JohnGoober <crappyguy123@gmail.com>
2021-09-28 01:49:19 -0400

    Update Player.lua

--- Player.lua
+++ Player.lua
@@ -1,31 +1,12 @@
 local Player = {}
 
-local LocalPlayer = game.Players.LocalPlayer.Character
-
 do 
     function Player.__index(t, i)
         return Player[i] or rawget(t, i) or rawget(t, "Instance")[i]
     end
 
     function Player.Create(player)
-        local TempPlayer = { Instance = player, Info = {} }
-
-        local function PositionChanged()
-            if LocalPlayer.Character then
-                TempPlayer.Info.Distance = (player.Character.PrimaryPart.Position - LocalPlayer.Character.PrimaryPart.Position).Magnitude
-                print(TempPlayer.Info.Distance)
-            end
-        end
-
-        local function CharacterAdded(character)
-            repeat wait() until character.PrimaryPart
-            character.PrimaryPart:GetPropertyChangedSignal("Position"):Connect(PositionChanged)
-        end
-        player.CharacterAdded:Connect(CharacterAdded)
-
-        if player.Character then
-            player.Character.PrimaryPart:GetPropertyChangedSignal("Position"):Connect(PositionChanged)
-        end
+        local TempPlayer = { Instance = player }
 
         return setmetatable(TempPlayer, Player)
     end
