46205703e0550206fb391037826c37a7d46721a5
JohnGoober <crappyguy123@gmail.com>
2021-09-28 01:54:41 -0400

    Update Player.lua

--- Player.lua
+++ Player.lua
@@ -2,11 +2,21 @@ local Player = {}
 
 do 
     function Player.__index(t, i)
-        return Player[i] or rawget(t, i) or rawget(t, "Instance")[i]
+        return Player[i] or rawget(t, i)
     end
 
     function Player.Create(player)
-        local TempPlayer = { Instance = player }
+        local TempPlayer = { Character = player.Character }
+
+        local function CharacterAdded(character)
+            TempPlayer.Character = character
+        end
+        player.CharacterAdded:Connect(CharacterAdded)
+
+        local function CharacterRemoved(character)
+            TempPlayer.Character = character
+        end
+        player.CharacterRemoved:Connect(CharacterRemoved)
 
         return setmetatable(TempPlayer, Player)
     end
