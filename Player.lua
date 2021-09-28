7abb8f2e0bfd41f38086e87fa24a9115cf78eb59
JohnGoober <crappyguy123@gmail.com>
2021-09-28 01:20:42 -0400

    Update Player.lua

--- Player.lua
+++ Player.lua
@@ -5,7 +5,7 @@ local OriginOffset = Vector3.new(0, 1.5, 0)
 
 do 
     function Player.Create(player, parent)
-        local TempPlayer = { Instance = player, UI = { Rectangle = {}, Dot = {}, Label = {} } }
+        local TempPlayer = { Instance = player, UI = { Rectangle = {} } }
         local UI = TempPlayer.UI
 
         UI.Esp = Instance.new("Frame")
@@ -16,13 +16,6 @@ do
         UI.Rectangle.Top = Instance.new("Frame")
         UI.Rectangle.Bottom = Instance.new("Frame")
 
-        UI.Dot.Head = Instance.new("Frame")
-        UI.Dot.UICorner = Instance.new("UICorner")
-        UI.Dot.UIStroke = Instance.new("UIStroke")
-
-        UI.Label.BottomLabel = Instance.new("TextLabel")
-        UI.Label.TextLabel = Instance.new("TextLabel")
-
         -- Properties
         do
             UI.Esp.Name = "Esp"
@@ -30,29 +23,6 @@ do
             UI.Esp.BackgroundTransparency = 1.000
             UI.Esp.BorderSizePixel = 0
             UI.Esp.AnchorPoint = Vector2.new(0.5, 0.5)
-            
-            UI.Label.BottomLabel.Name = "BottomLabel"
-            UI.Label.BottomLabel.Parent = UI.Esp
-            UI.Label.BottomLabel.BackgroundTransparency = 1.000
-            UI.Label.BottomLabel.Position = UDim2.new(0, 0, 1.05, 0)
-            UI.Label.BottomLabel.Size = UDim2.new(1, 0, 0, 0)
-            UI.Label.BottomLabel.Font = Enum.Font.Arial
-            UI.Label.BottomLabel.Text = "Bottom Label"
-            UI.Label.BottomLabel.TextSize = 10
-            UI.Label.BottomLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-            UI.Label.BottomLabel.TextStrokeTransparency = 0.750
-            UI.Label.BottomLabel.TextYAlignment = Enum.TextYAlignment.Top
-            
-            UI.Label.TextLabel.Parent = UI.Esp
-            UI.Label.TextLabel.BackgroundTransparency = 1.000
-            UI.Label.TextLabel.Position = UDim2.new(0, 0, -0.05, 0)
-            UI.Label.TextLabel.Size = UDim2.new(1, 0, 0, 0)
-            UI.Label.TextLabel.Font = Enum.Font.Arial
-            UI.Label.TextLabel.Text = "Top Label"
-            UI.Label.TextLabel.TextSize = 10
-            UI.Label.TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-            UI.Label.TextLabel.TextStrokeTransparency = 0.750
-            UI.Label.TextLabel.TextYAlignment = Enum.TextYAlignment.Bottom
             
             UI.Rectangle.Box.Name = "Box"
             UI.Rectangle.Box.Parent = UI.Esp
@@ -88,20 +58,6 @@ do
             UI.Rectangle.Bottom.BorderSizePixel = 0
             UI.Rectangle.Bottom.Position = UDim2.new(0, 0, 1, 0)
             UI.Rectangle.Bottom.Size = UDim2.new(1, 0, 0, 1)
-            
-            UI.Dot.Head.Name = "Head"
-            UI.Dot.Head.Parent = UI.Esp
-            UI.Dot.Head.AnchorPoint = Vector2.new(0.5, 0.5)
-            UI.Dot.Head.BackgroundTransparency = 1.000
-            UI.Dot.Head.Position = UDim2.new(0.5, 0, 0.25, 0)
-            UI.Dot.Head.Size = UDim2.new(0.1, 0, 0.1, 0)
-            UI.Dot.Head.SizeConstraint = Enum.SizeConstraint.RelativeXX
-            
-            UI.Dot.UICorner.CornerRadius = UDim.new(1, 0)
-            UI.Dot.UICorner.Parent = UI.Dot.Head
-
-            UI.Dot.UIStroke.Color = Color3.fromRGB(255, 255, 255)
-            UI.Dot.UIStroke.Parent = UI.Dot.Head
         end
 
         return setmetatable(TempPlayer, Player)
@@ -112,15 +68,17 @@ do
     end
 
     function Player:UpdateEsp()
-        if self.Character then
+        if self.Character  then
             local PrimaryPart = self.Character.PrimaryPart
             local Head = self.Character:WaitForChild("Head")
 
             local PrimaryPartPosition, PrimaryPartVisible = Camera:WorldToScreenPoint(PrimaryPart.Position)
+            local HeadPosition, HeadVisible = Camera:WorldToScreenPoint(Head.Position)
             local OriginPosition, OriginVisible = Camera:WorldToScreenPoint(PrimaryPart.Position - OriginOffset)
 
+            self.UI.Esp.Visible = OriginVisible and HeadPosition
+
             if self.UI.Esp.Visible then
-                local HeadPosition, HeadVisible = Camera:WorldToScreenPoint(Head.Position)
                 local HeadOriginDelta = (OriginPosition.Y - HeadPosition.Y) * 1.5
                 local HeadEspDifference = Vector2.new(HeadPosition.X, HeadPosition.Y) - self.UI.Esp.AbsolutePosition
 
